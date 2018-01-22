2f6d200ec9027d3827e99ff79ef64f9e5f261624
Vadim Godunko <godunko@gmail.com>
2018-01-23 01:27:55 +0300

    Move shader's code to program package.

--- examples/pyramid/pyramid-programs.adb
+++ examples/pyramid/pyramid-programs.adb
@@ -37,97 +37,41 @@
 -- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
 --                                                                          --
 ------------------------------------------------------------------------------
-
-with League.Strings;
 with League.Characters.Latin;
+with League.Strings;
 
-with GLFW; use GLFW;
+package body Pyramid.Programs is
 
-with OpenGL.Contexts.Internals;
-with OpenGL.Contexts;
-with OpenGL.Generic_Buffers;
-with OpenGL.Programs;
-with OpenGL.Shaders;
-with OpenGL.Textures;
-
-with Pyramid.Programs;
-
-procedure Pyramid.Driver is
-   use type OpenGL.GLfloat;
    use type League.Strings.Universal_String;
 
-   type Float_Array is array (Positive range <>) of OpenGL.GLfloat;
+   Text_V : League.Strings.Universal_String :=
+     League.Strings.To_Universal_String
+       ("#version 130") & League.Characters.Latin.Line_Feed &
+     "in vec3 vp;" &
+     "in vec2 tc;" &
+     "out vec2 TexCoord0;" &
+     "void main() {" &
+     "  gl_Position = vec4(vp, 1.0);" &
+     "  TexCoord0 = tc;" &
+     "}";
+   Text_F : League.Strings.Universal_String :=
+     League.Strings.To_Universal_String
+       ("#version 130") & League.Characters.Latin.Line_Feed &
+     "in vec2 TexCoord0;" &
+     "out vec4 frag_colour;" &
+     "uniform sampler2D gSampler;" &
+     "void main() {" &
+     "  frag_colour = texture2D(gSampler, TexCoord0.xy) +vec4(0.0, 0.0, 0.2, 0.2);" &
+     "}";
 
-   package Float_Buffers is new OpenGL.Generic_Buffers
-     (OpenGL.GLfloat, Positive, Float_Array);
+   ----------------
+   -- Initialize --
+   ----------------
 
-   VP : League.Strings.Universal_String :=
-     League.Strings.To_Universal_String ("vp");
-   TC : League.Strings.Universal_String :=
-     League.Strings.To_Universal_String ("tc");
-   Context : OpenGL.Contexts.OpenGL_Context;
-   Points  : Float_Array (1 .. 15) :=
-     (0.0, 0.5, 0.0,    0.5, 1.0,
-      0.5, -0.5, 0.0,   1.0, 0.0,
-      -0.5, -0.5, 0.0,  0.0, 0.0);
-   Buffer   : Float_Buffers.OpenGL_Buffer (OpenGL.Vertex);
-   Program  : Pyramid.Programs.Pyramid_Program;
-   Texture  : OpenGL.Textures.OpenGL_Texture (OpenGL.Texture_2D);
-   Img      : array (1 .. 9, 1 .. 3) of OpenGL.GLubyte :=
-     ((255, 0, 0),   (127, 0, 0), (0, 0, 127),
-      (127, 255, 0), (0, 127, 0), (0, 0, 127),
-      (32, 0, 255),  (0, 0, 127), (0,0, 127));
-begin
-   Context.Create;
-   Program.Initialize;
-
-   Context.Functions.Enable (OpenGL.GL_DEPTH_TEST);
-   Buffer.Create;
-   Buffer.Bind;
-   Buffer.Allocate (Points);
-
-   Program.Bind;
-
-   Program.Set_Uniform_Value
-     (Program.Uniform_Location
-        (League.Strings.To_Universal_String ("gSampler")), 0);
-
-   Texture.Create;
-   Texture.Bind;
-   Texture.Set_Image_2D
-     (0, OpenGL.GL_RGB, 3, 3, OpenGL.GL_UNSIGNED_BYTE, Img'Address);
-   Texture.Set_Parameter (OpenGL.GL_TEXTURE_MIN_FILTER, OpenGL.GL_LINEAR);
-   Texture.Set_Parameter (OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_LINEAR);
-
-   declare
-      use type OpenGL.GLbitfield;
-
-      Clear_Flag : OpenGL.Clear_Buffer_Mask :=
-        OpenGL.GL_DEPTH_BUFFER_BIT + OpenGL.GL_COLOR_BUFFER_BIT;
-
-      Window : GLFWwindow_Access :=
-        OpenGL.Contexts.Internals.Current_GLFW_Context;
+   procedure Initialize (Self : in out Pyramid_Program'Class) is
    begin
-      while glfwWindowShouldClose (Window) in 0 loop
-         Context.Functions.Clear (Clear_Flag);
-         Program.Enable_Attribute_Array (Program.Attribute_Location (VP));
-         Program.Enable_Attribute_Array (Program.Attribute_Location (TC));
-         Program.Set_Attribute_Buffer
-           (Program.Attribute_Location (VP),
-            OpenGL.GL_FLOAT,
-            Tuple_Size => 3, Offset => 0, Stride => 20);
+      Self.Add_Shader_From_Source_Code (OpenGL.Vertex, Text_V);
+      Self.Add_Shader_From_Source_Code (OpenGL.Fragment, Text_F);
+   end Initialize;
 
-         Program.Set_Attribute_Buffer
-           (Program.Attribute_Location (TC),
-            OpenGL.GL_FLOAT,
-            Tuple_Size => 2, Offset => 12, Stride => 20);
-
-         Texture.Bind;
-         Context.Functions.Draw_Arrays (OpenGL.GL_TRIANGLES, 0, 3);
-
-         glfwPollEvents;
-         glfwSwapBuffers (Window);
-      end loop;
-   end;
-
-end Pyramid.Driver;
+end Pyramid.Programs;
--- examples/pyramid/pyramid-programs.ads
+++ examples/pyramid/pyramid-programs.ads
@@ -37,97 +37,19 @@
 -- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
 --                                                                          --
 ------------------------------------------------------------------------------
-
-with League.Strings;
-with League.Characters.Latin;
-
-with GLFW; use GLFW;
-
-with OpenGL.Contexts.Internals;
-with OpenGL.Contexts;
-with OpenGL.Generic_Buffers;
 with OpenGL.Programs;
-with OpenGL.Shaders;
-with OpenGL.Textures;
 
-with Pyramid.Programs;
+package Pyramid.Programs is
 
-procedure Pyramid.Driver is
-   use type OpenGL.GLfloat;
-   use type League.Strings.Universal_String;
+   type Pyramid_Program is new OpenGL.Programs.OpenGL_Program with private;
 
-   type Float_Array is array (Positive range <>) of OpenGL.GLfloat;
+   procedure Initialize (Self : in out Pyramid_Program'Class);
+   --  Initialize program object.
 
-   package Float_Buffers is new OpenGL.Generic_Buffers
-     (OpenGL.GLfloat, Positive, Float_Array);
+private
 
-   VP : League.Strings.Universal_String :=
-     League.Strings.To_Universal_String ("vp");
-   TC : League.Strings.Universal_String :=
-     League.Strings.To_Universal_String ("tc");
-   Context : OpenGL.Contexts.OpenGL_Context;
-   Points  : Float_Array (1 .. 15) :=
-     (0.0, 0.5, 0.0,    0.5, 1.0,
-      0.5, -0.5, 0.0,   1.0, 0.0,
-      -0.5, -0.5, 0.0,  0.0, 0.0);
-   Buffer   : Float_Buffers.OpenGL_Buffer (OpenGL.Vertex);
-   Program  : Pyramid.Programs.Pyramid_Program;
-   Texture  : OpenGL.Textures.OpenGL_Texture (OpenGL.Texture_2D);
-   Img      : array (1 .. 9, 1 .. 3) of OpenGL.GLubyte :=
-     ((255, 0, 0),   (127, 0, 0), (0, 0, 127),
-      (127, 255, 0), (0, 127, 0), (0, 0, 127),
-      (32, 0, 255),  (0, 0, 127), (0,0, 127));
-begin
-   Context.Create;
-   Program.Initialize;
+   type Pyramid_Program is new OpenGL.Programs.OpenGL_Program with record
+      null;
+   end record;
 
-   Context.Functions.Enable (OpenGL.GL_DEPTH_TEST);
-   Buffer.Create;
-   Buffer.Bind;
-   Buffer.Allocate (Points);
-
-   Program.Bind;
-
-   Program.Set_Uniform_Value
-     (Program.Uniform_Location
-        (League.Strings.To_Universal_String ("gSampler")), 0);
-
-   Texture.Create;
-   Texture.Bind;
-   Texture.Set_Image_2D
-     (0, OpenGL.GL_RGB, 3, 3, OpenGL.GL_UNSIGNED_BYTE, Img'Address);
-   Texture.Set_Parameter (OpenGL.GL_TEXTURE_MIN_FILTER, OpenGL.GL_LINEAR);
-   Texture.Set_Parameter (OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_LINEAR);
-
-   declare
-      use type OpenGL.GLbitfield;
-
-      Clear_Flag : OpenGL.Clear_Buffer_Mask :=
-        OpenGL.GL_DEPTH_BUFFER_BIT + OpenGL.GL_COLOR_BUFFER_BIT;
-
-      Window : GLFWwindow_Access :=
-        OpenGL.Contexts.Internals.Current_GLFW_Context;
-   begin
-      while glfwWindowShouldClose (Window) in 0 loop
-         Context.Functions.Clear (Clear_Flag);
-         Program.Enable_Attribute_Array (Program.Attribute_Location (VP));
-         Program.Enable_Attribute_Array (Program.Attribute_Location (TC));
-         Program.Set_Attribute_Buffer
-           (Program.Attribute_Location (VP),
-            OpenGL.GL_FLOAT,
-            Tuple_Size => 3, Offset => 0, Stride => 20);
-
-         Program.Set_Attribute_Buffer
-           (Program.Attribute_Location (TC),
-            OpenGL.GL_FLOAT,
-            Tuple_Size => 2, Offset => 12, Stride => 20);
-
-         Texture.Bind;
-         Context.Functions.Draw_Arrays (OpenGL.GL_TRIANGLES, 0, 3);
-
-         glfwPollEvents;
-         glfwSwapBuffers (Window);
-      end loop;
-   end;
-
-end Pyramid.Driver;
+end Pyramid.Programs;

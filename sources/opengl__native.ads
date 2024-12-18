cf2e9fc9a3d8a6e4a119fb2d717734827242cd2e
Vadim Godunko <vgodunko@gmail.com>
2024-12-18 13:51:31 +0400

    Fix compiler errors.

--- sources/opengl__native.ads
+++ sources/opengl__native.ads
@@ -1,42 +1,8 @@
-------------------------------------------------------------------------------
---                                                                          --
---                       Ada binding for OpenGL/WebGL                       --
---                                                                          --
---                        Runtime Library Component                         --
---                                                                          --
-------------------------------------------------------------------------------
---                                                                          --
--- Copyright © 2018-2020, Vadim Godunko <vgodunko@gmail.com>                --
--- All rights reserved.                                                     --
---                                                                          --
--- Redistribution and use in source and binary forms, with or without       --
--- modification, are permitted provided that the following conditions       --
--- are met:                                                                 --
---                                                                          --
---  * Redistributions of source code must retain the above copyright        --
---    notice, this list of conditions and the following disclaimer.         --
---                                                                          --
---  * Redistributions in binary form must reproduce the above copyright     --
---    notice, this list of conditions and the following disclaimer in the   --
---    documentation and/or other materials provided with the distribution.  --
---                                                                          --
---  * Neither the name of the Vadim Godunko, IE nor the names of its        --
---    contributors may be used to endorse or promote products derived from  --
---    this software without specific prior written permission.              --
---                                                                          --
--- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
--- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
--- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
--- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
--- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
--- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
--- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
--- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
--- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
--- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
--- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
---                                                                          --
-------------------------------------------------------------------------------
+--
+--  Copyright (C) 2018-2024, Vadim Godunko
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
 
 with Interfaces;
 
@@ -142,14 +108,14 @@ package OpenGL is
        with Convention => Fortran;
 
    type GLfixed_Matrix_2x2 is
-     array (Positive range 1 .. 2, Positive range 1 .. 2) of aliased GLfixed
-       with Convention => Fortran;
+     array (Positive range 1 .. 2, Positive range 1 .. 2) of aliased GLfixed;
+       --  with Convention => Fortran;
    type GLfixed_Matrix_3x3 is
-     array (Positive range 1 .. 3, Positive range 1 .. 3) of aliased GLfixed
-       with Convention => Fortran;
+     array (Positive range 1 .. 3, Positive range 1 .. 3) of aliased GLfixed;
+       --  with Convention => Fortran;
    type GLfixed_Matrix_4x4 is
-     array (Positive range 1 .. 4, Positive range 1 .. 4) of aliased GLfixed
-       with Convention => Fortran;
+     array (Positive range 1 .. 4, Positive range 1 .. 4) of aliased GLfixed;
+       --  with Convention => Fortran;
 
    type GLfloat_Matrix_2x2 is
      array (Positive range 1 .. 2, Positive range 1 .. 2) of aliased GLfloat
@@ -652,5 +618,9 @@ private
    GL_STENCIL_ATTACHMENT          : constant GLenum := 16#8D20#;
    GL_STENCIL_INDEX8              : constant GLenum := 16#8D48#;
    GL_RGB565                      : constant GLenum := 16#8D62#;
+
+   pragma Convention (Fortran, GLfixed_Matrix_2x2);
+   pragma Convention (Fortran, GLfixed_Matrix_3x3);
+   pragma Convention (Fortran, GLfixed_Matrix_4x4);
 
 end OpenGL;
